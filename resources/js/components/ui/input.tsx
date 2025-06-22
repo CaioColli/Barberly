af280b94f33d138c0d711b393124d25978275058
Caio <caiocolli22@gmail.com>
2025-06-22 10:44:06 -0300

    style modo dark em telas criadas

--- resources/js/components/ui/input.tsx
+++ resources/js/components/ui/input.tsx
@@ -8,7 +8,7 @@ function Input({ className, type, ...props }: React.ComponentProps<"input">) {
       type={type}
       data-slot="input"
       className={cn(
-        'rounded-[8px] border-[1px] border-[var(--custom-black)] py-[8px] pl-[8px] text-[var(--custom-black)] placeholder:text-[var(--custom-gray)] shadow-[4px_5px_0_0_var(--custom-black)] outline-none focus:shadow-[6px_7px_0_0_var(--custom-black)] transition-all duration-200',
+        'rounded-[8px] border-[1px] border-[var(--custom-black)] py-[8px] pl-[8px] text-[var(--custom-black)] placeholder:text-[var(--custom-gray)] shadow-[4px_5px_0_0_var(--custom-black)] outline-none focus:shadow-[6px_7px_0_0_var(--custom-black)] transition-all duration-200 bg-[#ffffff]',
         className
       )}
       {...props}
--- resources/js/layouts/auth/auth-layout.tsx
+++ resources/js/layouts/auth/auth-layout.tsx
@@ -9,8 +9,8 @@ interface AuthProps {
 export const AuthBodyLayout = ({ children, title, span }: AuthProps) => {
     return (
         <>
-            <section className="min-h-dvh flex flex-col items-center bg-[var(--custom-black-bg)] px-[24px] lg:px-[32px]">
-                <header className="py-[64px] flex flex-col justify-start w-full">
+            <section className="min-h-dvh flex flex-col items-center bg-background-primary px-[24px] lg:px-[32px]">
+                <header className="py-[64px] flex flex-col justify-start w-full text-primary">
                     <h1 className="text-[32px] lg:text-[48px] font-azonix max-w-[600px]">
                         {title}
                     </h1>
@@ -20,7 +20,7 @@ export const AuthBodyLayout = ({ children, title, span }: AuthProps) => {
                     </span>
                 </header>
 
-                <main className="w-screen bg-[var(--custom-middle-white)] flex-1 flex flex-col rounded-t-[16px]">
+                <main className="w-screen bg-background-third flex-1 flex flex-col rounded-t-[16px] py-[48px] px-[24px] lg:px-[32px] relative">
                     {children}
                 </main>
                 
