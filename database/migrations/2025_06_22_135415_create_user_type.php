70b21424a62b24b0c76d8fa5e6983068188cec90
Caio <caiocolli22@gmail.com>
2025-06-27 23:13:02 -0300

    refactor type para user_type na migration users_type

--- database/migrations/2025_06_22_135415_create_user_type.php
+++ database/migrations/2025_06_22_135415_create_user_type.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users_type', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->string('type');
+            $table->string('user_type');
             $table->timestamps();
         });
     }
