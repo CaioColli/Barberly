a014763279d24dd96fd32114744612bc4984e061
Caio <caiocolli22@gmail.com>
2025-07-16 22:12:02 -0300

    style padding footer

--- resources/js/components/ui/footer.tsx
+++ resources/js/components/ui/footer.tsx
@@ -3,7 +3,7 @@ import { IoLogoInstagram } from "react-icons/io5";
 
 export const Footer = () => {
     return (
-        <footer className='w-full flex flex-col items-center py-[32px] gap-[16px] lg:flex-row lg:justify-between'>
+        <footer className='w-full flex flex-col items-center py-[32px] gap-[16px] lg:flex-row lg:px-8 lg:justify-between'>
             <div className="text-[var(--custom-dark-gray)]">
                 <h1 className='text-[32px] font-azonix'>Barberly</h1>
                 <span className='text-lg'>
@@ -11,16 +11,14 @@ export const Footer = () => {
                 </span>
             </div>
 
-            <div>
-                <nav className='flex flex-col gap-[16px] items-center lg:items-start text-primary'>
-                    <h2 className='text-xl'>Nossas redes sociais</h2>
-                    <ul className='flex gap-[16px]'>
-                        <li> <FaTwitter /> </li>
-                        <li> <FaFacebookSquare /> </li>
-                        <li> <IoLogoInstagram /> </li>
-                    </ul>
-                </nav>
-            </div>
+            <nav className='flex flex-col gap-[16px] items-center lg:items-start text-primary'>
+                <h2 className='text-xl'>Nossas redes sociais</h2>
+                <ul className='flex gap-[16px]'>
+                    <li> <FaTwitter /> </li>
+                    <li> <FaFacebookSquare /> </li>
+                    <li> <IoLogoInstagram /> </li>
+                </ul>
+            </nav>
         </footer>
     )
 }
